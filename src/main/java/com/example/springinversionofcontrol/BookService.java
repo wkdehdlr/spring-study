0cbed6ccae5c433faca1047298421da6be5e89bb
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-12-15 21:58:57 +0900

    bean 설정을 xml이 아닌 자바로 하기위한 방법 클래스에 @Configuration annotation을 달고 @Bean annotation으로 객체간 의존성을 명시한다. ApplicationContext context =                new ClassPathXmlApplicationContext(Configuration클래스.class) 위와 같은 방법으로 context를 생성하면 의존성이 주입된다.

--- src/main/java/com/example/springinversionofcontrol/BookService.java
+++ src/main/java/com/example/springinversionofcontrol/BookService.java
@@ -1,12 +1,7 @@
 package com.example.springinversionofcontrol;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
 public class BookService {
 
-  @Autowired
   BookRepository bookRepository;
 
   public void setBookRepository(BookRepository bookRepository) {
--- src/main/java/com/example/springinversionofcontrol/SpringiocApplication.java
+++ src/main/java/com/example/springinversionofcontrol/SpringiocApplication.java
@@ -2,12 +2,13 @@ package com.example.springinversionofcontrol;
 
 import java.util.Arrays;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringiocApplication {
 
   public static void main(String[] args) {
-    ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+    ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
     String[] beanDefinitionNames = context.getBeanDefinitionNames();
     System.out.println(Arrays.toString(beanDefinitionNames));
     BookService bookService = (BookService) context.getBean("bookService");
