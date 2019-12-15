d9740c87470df9df78641da2ae01e0dd8d60b4cc
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-12-15 20:42:32 +0900

    context:component-scan을 활용하는 법 application.xml에 빈을 하나하나 등록하지 않고 component-scan을 통해서 해당 패키지를 스캐닝하며 bean을 등록한다. 스캐닝하면서 @Component annotation 을 찾아 bean으로 등록한다 @Component annotation에는 @Repository @Service @Controller @Configuration 가 있다.

--- src/main/java/com/example/springinversionofcontrol/BookService.java
+++ src/main/java/com/example/springinversionofcontrol/BookService.java
@@ -1,7 +1,12 @@
 package com.example.springinversionofcontrol;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
+  @Autowired
   BookRepository bookRepository;
 
   public void setBookRepository(BookRepository bookRepository) {
