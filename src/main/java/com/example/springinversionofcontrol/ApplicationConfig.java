2eb284f7ccffbdce50bf72b859ac5c08b9472d37
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-12-15 22:18:11 +0900

    SpringBootApplication annotation 사실상 이 annotation만 있으면 ApplicationConfig 클래스가 없어도 되고 ApplicationContext를 직접 만들어서 쓰지 않아도 된다. SpringBootApplication annotation을 까보면 안에서 @ComponentScan, @Configuration 이 이미 세팅이 되어있다. 따라서 @SpringBootApplication 자체가 사실상 bean 설정 파일이 되는것이다.

--- src/main/java/com/example/springinversionofcontrol/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.springinversionofcontrol;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackageClasses = SpringiocApplication.class)
-public class ApplicationConfig {
-}
--- src/main/java/com/example/springinversionofcontrol/SpringiocApplication.java
+++ src/main/java/com/example/springinversionofcontrol/SpringiocApplication.java
@@ -1,18 +1,9 @@
 package com.example.springinversionofcontrol;
 
-import java.util.Arrays;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringiocApplication {
-
   public static void main(String[] args) {
-    ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-
-    String[] beanDefinitionNames = context.getBeanDefinitionNames();
-    System.out.println(Arrays.toString(beanDefinitionNames));
-    BookService bookService = (BookService) context.getBean("bookService");
-    System.out.println(bookService.bookRepository != null);
   }
-
 }
