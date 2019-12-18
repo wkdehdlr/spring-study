8d8d4222b5d95df9a777285681a0cb020066a64c
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-12-18 21:21:20 +0900

    경우의 수 ㅁ해당 타입의 빈이 여러 개인 경우   ㅁ모든 빈 주입받기

--- src/main/java/com/example/springinversionofcontrol/BookService.java
+++ src/main/java/com/example/springinversionofcontrol/BookService.java
@@ -1,16 +1,16 @@
 package com.example.springinversionofcontrol;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
-  @Autowired @Qualifier("doikBookRepository")
-  BookRepository bookRepository;
+  @Autowired
+  List<BookRepository> bookRepository;
 
   public void printBookRepository(){
-    System.out.print(bookRepository.getClass());
+    this.bookRepository.forEach(System.out::println);
   }
 }
