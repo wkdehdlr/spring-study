24eea4f0dab2d7296361385c0a16b055dbbd9c98
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-12-22 14:49:04 +0900

    ClassPathXmlApplicationContext -> class path 기준으로 설정파일을 찾는다 FileSystemXmlApplicationContext -> file system 경로기준으로 설정파일을 찾는다 내부적으로는 둘이 다른 구현체를 사용한다.

--- src/main/java/com/example/demo3/AppRunner.java
+++ src/main/java/com/example/demo3/AppRunner.java
@@ -4,12 +4,14 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    ApplicationContext ctx = new ClassPathXmlApplicationContext("strstrstr.xml");
+    ApplicationContext ctx = new FileSystemXmlApplicationContext("xxx.xml");
+
   }
 }
