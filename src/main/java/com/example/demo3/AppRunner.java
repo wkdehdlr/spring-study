3408f67a0810921da36d1a7ccd0f9ca1c22103f0
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-12-22 15:10:58 +0900

    접두어없이 리소스를 가져오면 기본 ServletContextResource에서 리소스를 가져오는데 (웹어플리케이션루트, 즉 context root 부터 찾아서 가져옴) 스프링부트가 띄워주는 기본적인 내장형 톰캣에는 context path가 지정되어있지 않기 때문에 context path부터 찾으면 리소스를 찾을 수 없다.

--- src/main/java/com/example/demo3/AppRunner.java
+++ src/main/java/com/example/demo3/AppRunner.java
@@ -16,7 +16,8 @@ public class AppRunner implements ApplicationRunner {
   @Override
   public void run(ApplicationArguments args) throws Exception {
     System.out.println(resourceLoader.getClass());
-    Resource resource = resourceLoader.getResource("classpath:text.txt");
+    Resource resource = resourceLoader.getResource("text.txt");
     System.out.println(resource.getClass());
+    System.out.println(resource.exists());
   }
 }
