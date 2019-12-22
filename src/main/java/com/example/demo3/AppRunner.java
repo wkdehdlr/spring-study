b66e605aadeef531fb6d3d0f48f76aaa7d186067
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-12-22 14:51:19 +0900

    항상 모든 resource를 file로 가져올 수 있는건 아니다.

--- src/main/java/com/example/demo3/AppRunner.java
+++ src/main/java/com/example/demo3/AppRunner.java
@@ -1,17 +1,24 @@
 package com.example.demo3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
+
+  @Autowired
+  ResourceLoader resourceLoader;
+
   @Override
   public void run(ApplicationArguments args) throws Exception {
     ApplicationContext ctx = new FileSystemXmlApplicationContext("xxx.xml");
-
+    Resource resource = resourceLoader.getResource("classpath:text.txt");
+    resource.getFile();
   }
 }
