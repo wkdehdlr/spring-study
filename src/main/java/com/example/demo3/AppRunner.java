4cbf753d59021b1c5a20d5f94c7396bba0173fed
shareitDOIK <doik.jang@schoolsharing.co.kr>
2019-12-22 15:22:04 +0900

    리소스를 가져올 때 접두어를 생략하면 해당 리소스가 만들어진 context타입에 따라서 가져온다. ㅁClassPathXmlApplicationContext로 만들었으면 getResource에서 "classpath" 안써도 classpath로 부터 가져오고 ㅁFileSystemResource로 만들면 getResource에서 "file:///" 안써도 file system으로 부터 가져온다. 하지만 getResoruce에서 classpath, file 접두어를 아예 명시함으로써 리소스 타입을 강제해서 가져올 수 있다. -> 추천함

--- src/main/java/com/example/demo3/AppRunner.java
+++ src/main/java/com/example/demo3/AppRunner.java
@@ -3,7 +3,9 @@ package com.example.demo3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +17,12 @@ public class AppRunner implements ApplicationRunner {
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    Resource resource1 = resourceLoader.getResource("classpath:text.txt");
-    Resource resource2 = resourceLoader.getResource("file:///text.txt");
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("xxx.xml");
+    ctx.getResource("text.txt");
+    ctx.getResource("file:///text.txt");
+
+    ApplicationContext ctxx = new FileSystemXmlApplicationContext("xxx.xml");
+    ctxx.getResource("text.path");
+    ctxx.getResource("classpath:text.txt");
   }
 }
