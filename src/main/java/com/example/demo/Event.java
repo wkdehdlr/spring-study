2c4213cb0425fbce8afb3b0c2a0888194aec435c
wkdehdlr <wkdehdlr@gmail.com>
2019-12-24 14:26:50 +0900

    Validator를 직접 사용하는게 아니라 스프링이 제공해주는 Validator중에 LocalValidatorFactoryBean을 자동으로 등록함으로써 사용가능 (Bean Validation annotation을 지원하는 Validator) annotation으로 검증할만한 간단한것들은 Validator를 직접 구현하지 않고 검증할 수 있음

---
+++ src/main/java/com/example/demo/Event.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class Event {
+
+    Integer id;
+    @NotEmpty
+    String title;
+
+    @NotNull  @Min(0)
+    Integer limit;
+
+    @Email
+    String email;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
