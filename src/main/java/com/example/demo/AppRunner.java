03740d5124af9dd2ec8c979c4979ea7094edf5c1
wkdehdlr <wkdehdlr@gmail.com>
2019-12-24 15:23:03 +0900

    Validator를 직접 사용하는게 아니라 스프링이 제공해주는 Validator중에 LocalValidatorFactoryBean을 자동으로 등록함으로써 사용가능 (Bean Validation annotation을 지원하는 Validator) annotation으로 검증할만한 간단한것들은 Validator를 직접 구현하지 않고 검증할 수 있음

--- src/main/java/com/example/demo/AppRunner.java
+++ src/main/java/com/example/demo/AppRunner.java
@@ -1,23 +1,31 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
+    @Autowired
+    Validator validator;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(validator.getClass());
+
         Event event = new Event();
-        EventValidator eventValidator = new EventValidator();
+        event.setLimit(-1);
+        event.setEmail("aaa2");
         Errors errors = new BeanPropertyBindingResult(event, "event");
 
-        eventValidator.validate(event, errors);
+        validator.validate(event, errors);
         System.out.println(errors.hasErrors());
 
         errors.getAllErrors().forEach(e -> {
