6b9996bee50f2e79da57218bdd3c032814495847
wkdehdlr <wkdehdlr@gmail.com>
2019-12-25 16:12:39 +0900

    기존코드를 건드리지 않고 공통된 작업을 수행할 수 있음 @Around("execution(* com.example..*.EventService.*(..)))") 모든 메소드에 적용이 되기 때문에 수정이 필요함

---
+++ src/main/java/com/example/demo/PerfAspect.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerfAspect {
+
+    @Around("execution(* com.example..*.EventService.*(..)))")
+    public Object logPerf(ProceedingJoinPoint pjp) throws Throwable {
+        long begin = System.currentTimeMillis();
+        Object reVal = pjp.proceed();
+        System.out.println(System.currentTimeMillis() - begin);
+        return reVal;
+    }
+}
