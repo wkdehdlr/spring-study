849cecdb9e40829afdf153e4dd0b5ca782b3849c
wkdehdlr <wkdehdlr@gmail.com>
2019-12-25 16:24:11 +0900

    @Before

--- src/main/java/com/example/demo/PerfAspect.java
+++ src/main/java/com/example/demo/PerfAspect.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,5 +16,10 @@ public class PerfAspect {
         Object reVal = pjp.proceed();
         System.out.println(System.currentTimeMillis() - begin);
         return reVal;
+    }
+
+    @Before("bean(simpleEventService)")
+    public void hello(){
+        System.out.println("hello");
     }
 }
