1fdcd2eb3bc00ae3d3f4d5a3f85bf253885327b2
wkdehdlr <wkdehdlr@gmail.com>
2019-12-25 21:16:10 +0900

    Null-safety - 파라미터나 리턴타입에 annotation을 붙여서 컴파일타임에 Null처리가 가능 - package 레벨에 annotation을 달아서 모든 파라미터와 리턴타입에 기본적으로 NonNull세팅을 할 수 있고 특수한 경우에만 Nullable로 작업하는 경우도 있음

---
+++ src/main/java/com/example/demo/EventService.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    @NonNull
+    public String createEvent(@NonNull String name){
+        return null;
+    }
+}
