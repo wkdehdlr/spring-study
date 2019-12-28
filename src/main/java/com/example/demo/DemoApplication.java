1d869d6c54617efa56aafaa4d8145f94bdf7b4c0
wkdehdlr <wkdehdlr@gmail.com>
2019-12-28 16:01:21 +0900

    boot03-3-EnableAutoConfiguration * ComponentScan     * Component라는 annotation을 가진놈들을 찾아 빈으로 등록함     * @Configuration, @Repository, @Service, @Controller, @RestController * EnableAutoConfiguration     * Spring-meta 파일     * Java-resource-meta -> spring.factories에 설정파일들을 빈으로 등록한다     * ConditionalOnXXXYYYZZZ         * 조건에 따라 빈으로 등록함

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-@SpringBootConfiguration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
