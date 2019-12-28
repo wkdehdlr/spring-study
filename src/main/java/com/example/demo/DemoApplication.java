8fcfb8b1daab9f68f4537cae55da97365d5fd56b
wkdehdlr <wkdehdlr@gmail.com>
2019-12-28 15:36:42 +0900

    boot03-3-EnableAutoConfiguration ㅁSpringBootApplication annotation은 아래 3개로 이루어짐  - SpringBootConfiguration  - ComponentScan  - EnableAutoConfiguration ㅁ부트는 빈을 2번 등록한다 - ComponentScan으로 등록 - EnableAutoConfigutarion으로 등록 - EnableAutoConfiguration이 ServletWebServerFactory 빈을 만들어서 등록함 ->따라서 부트 기본실행이 WebApplication으로 실행이 가능했던 것 ->WebApplication은 ServletWebServerFactory이 필요하다!

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootConfiguration
+@ComponentScan
+@EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
