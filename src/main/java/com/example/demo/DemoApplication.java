6f10bc7a0cd606259a5a8e9f3be033a6a6281d16
wkdehdlr <wkdehdlr@gmail.com>
2019-12-29 17:00:48 +0900

    ㅁHTTP2 * 기본적으로 ssl이 설정되어있어야함 * Application.properties     * Server.http2.enabled=true * undertow는 HTTPS만 적용되어 있으면 추가설정 따로 필요없음

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,7 @@
 package com.example.demo;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,18 +16,5 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Bean
-	public ServletWebServerFactory serverFactory(){
-		TomcatServletWebServerFactory tomcatServletWebServerFactory  = new TomcatServletWebServerFactory();
-		tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createStandardConnector());
-		return tomcatServletWebServerFactory;
-	}
-
-	private Connector createStandardConnector(){
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setPort(8080);
-		return connector;
 	}
 }
