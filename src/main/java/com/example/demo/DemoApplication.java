c6ded5a332408e4c5e0151c38f284affb78cc5ad
wkdehdlr <wkdehdlr@gmail.com>
2019-12-29 16:45:47 +0900

    ㅁHTTPS & HTTP - HTTP와 HTTPS 모두 받도록 설정

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +22,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory serverFactory(){
+		TomcatServletWebServerFactory tomcatServletWebServerFactory  = new TomcatServletWebServerFactory();
+		tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcatServletWebServerFactory;
+	}
+
+	private Connector createStandardConnector(){
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080);
+		return connector;
+	}
 }
