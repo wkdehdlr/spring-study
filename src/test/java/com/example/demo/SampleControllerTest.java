b7be9f0fd6251d0f67c604f9581b4850fdc34a54
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 16:42:03 +0900

    * RANDON_PORT, DEFINED_PORT: Servlet이  뜸     * MockMvc를 쓰는게 아니라 테스트용 RestTemplate이나 WebClient를 써야함

--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -3,30 +3,22 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    TestRestTemplate testRestTemplate;
 
     @Test
     public void hello() throws Exception{
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("doik"))
-                .andDo(print());
+        String object = testRestTemplate.getForObject("/hello", String.class);
+        assertThat(object).isEqualTo("doik");
     }
-
 }
