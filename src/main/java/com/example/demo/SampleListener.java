ce5975807ec0f9d88478d047eb77573e64021ffc
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 12:04:00 +0900

    * ApplicationContext가 만들어지기 전, 후 이벤트 처리가 다르다     * 처리할 이벤트가 ApplicationContext 생성전에 발생한다면 해당 이벤트리스너를 빈으로 등록하더라도 동작하지 않는다

---
+++ src/main/java/com/example/demo/SampleListener.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleListener implements ApplicationListener<ApplicationStartingEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent applicationStartingEvent) {
+        System.out.println("======================");
+        System.out.println("Application is Starting");
+        System.out.println("======================");
+    }
+}
