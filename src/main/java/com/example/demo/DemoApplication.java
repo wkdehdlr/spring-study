d0e72c3f1aa943d2fa29d9be2d97448d6f58d310
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 11:51:41 +0900

    boot04-2-Application ㅁ배너 변경 방법  - resource에 배너파일 추가  - 코딩으로 배너세팅

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        app.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+                out.println("======================");
+                out.println("coding banner");
+                out.println("======================");
+            }
+        });
+        app.run(args);
     }
-
 }
