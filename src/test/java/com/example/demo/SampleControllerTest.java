45dbc9aafc15b803d60204eac7661ad956a96abf
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 16:50:59 +0900

    예를들어 테스트가 너무커서 Controller에서 테스트를 끊고 싶을때 Service를 @MockBean으로 주입받으면 컨트롤러가 사용하는 서비스가 주입받은 MockService로 교체된다. 따라서 Mocking을 할 수 있다.

--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,8 +18,12 @@ public class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception{
+        when(mockSampleService.getName()).thenReturn("doik");
         String object = testRestTemplate.getForObject("/hello", String.class);
         assertThat(object).isEqualTo("doik");
     }
