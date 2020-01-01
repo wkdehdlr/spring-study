783e4e9eba8e7d0d8d586a2428fad3e8389a263d
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 13:18:24 +0900

    테스트에 있는 application.properties를 지운다 Test에서 @TestPropertySource annotation을 활용한다! 그럼 test에서 매번 application.properties에 속성을 추가할 필요없이 원하는 속성값만 변경해서 사용할 수 있다 -> 근데 이것도 속성이 많으면 문제가 됨

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,9 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(properties = "doik.name=doik")
 @SpringBootTest
 class DemoApplicationTests {
 
