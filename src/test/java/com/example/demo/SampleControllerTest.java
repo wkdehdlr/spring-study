0dc91cb2455945f5abbc50b29c9fb5b7cb19270b
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 18:26:33 +0900

    ㅁ슬라이스테스트 - SpringBootTest의 단위가 크다보니 쪼개서 원하는 테스트만 하고싶은 경우 - 의존성들이 깨지기 때문에 해당빈을 MockBean으로 등록하고 사용해야한다

--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -3,21 +3,29 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
+    @MockBean
+    SampleService sampleService;
+
     @Autowired
-    WebTestClient webTestClient;
+    MockMvc mockMvc;
+
 
     @Test
     public void hello() throws Exception{
-        webTestClient.get().uri("/hello").exchange().expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("doik");
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("doik"));
     }
 }
