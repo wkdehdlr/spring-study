90bc43df20f43966dc4b017d0c1fca95fc14252c
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 18:36:08 +0900

    ㅁ테스트유틸 * OutputCapture * TestPropertyValues * TestRestTemplate * ConfigFileApplicationContextInitializer

--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
@@ -15,6 +18,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
 
     @MockBean
     SampleService sampleService;
@@ -27,5 +33,11 @@ public class SampleControllerTest {
     public void hello() throws Exception{
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("doik"));
+
+        assertThat(outputCapture.toString())
+                .contains("holoman")
+                .contains("twosomeplace");
+
+
     }
 }
