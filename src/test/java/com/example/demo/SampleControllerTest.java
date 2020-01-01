dd70e734be16565c24b600f879a0a8e74a2eefe6
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 18:11:34 +0900

    WebTestClient 기존에 사용하던 RestClient는 동기 WebClient는 비동기 test코드에서도 WebTestClient를 사용해서 WebClient랑 동일, 비슷한 API를 사용할 수 있다

--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -4,27 +4,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerTest {
 
     @Autowired
-    TestRestTemplate testRestTemplate;
-
-    @MockBean
-    SampleService mockSampleService;
+    WebTestClient webTestClient;
 
     @Test
     public void hello() throws Exception{
-        when(mockSampleService.getName()).thenReturn("doik");
-        String object = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(object).isEqualTo("doik");
+        webTestClient.get().uri("/hello").exchange().expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("doik");
     }
 }
