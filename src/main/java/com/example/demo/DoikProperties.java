3e1e3fd7ca668f0b501d0fd8b7bd33bda4bb6d49
wkdehdlr <wkdehdlr@gmail.com>
2020-01-01 14:42:11 +0900

    ㅁ프로퍼티 값 검증 * @Validated * JSR-303(@NotNull, …)

--- src/main/java/com/example/demo/DoikProperties.java
+++ src/main/java/com/example/demo/DoikProperties.java
@@ -3,14 +3,20 @@ package com.example.demo;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 @Component
 @ConfigurationProperties("doik")
+@Validated
 public class DoikProperties {
+
+    @NotEmpty
     private String name;
+
     private int age;
 
     public String getName() {
