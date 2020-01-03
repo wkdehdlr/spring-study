94c45737997d4222e110cf99c9af9c7291ad514e
wkdehdlr <wkdehdlr@gmail.com>
2020-01-03 15:13:43 +0900

    boot04-15-ViewResolve * ContentsNegotiatingViewResolver     * 들어오는 accept 헤더에 따라 응답이 달라짐     * 요청이 들어오면 그 요청에 응답을 만들 수 있는 모든 View를 다 찾아서 accept 헤더랑 비교해서 최종적으로 선택함     * 경우에 따라서는 accept를 제공하지 않는 요청도 있음         * 그런 경우에 대비해서 format을 사용함             * format=pdf             * format=xml             * foramt=html ….
    XML 메시지 컨버터 추가하기
    <dependency>
       <groupId>com.fasterxml.jackson.dataformat</groupId>
       <artifactId>jackson-dataformat-xml</artifactId>
       <version>2.9.6</version>
    </dependency>

--- src/test/java/com/example/demo/user/UserControllerTest.java
+++ src/test/java/com/example/demo/user/UserControllerTest.java
@@ -42,4 +42,18 @@ public class UserControllerTest {
             .andExpect(jsonPath("$.password",
                     is(equalTo("123"))));
     }
+
+    @Test
+    public void createUser_XML() throws Exception {
+        String userJson = "{\"username\":\"doik\",\"password\":\"123\"}";
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_XML)
+                .content(userJson))
+            .andExpect(status().isOk())
+            .andExpect(xpath("/User/username")
+                    .string("doik"))
+            .andExpect(xpath("/User/password")
+                    .string("123"));
+    }
 }
