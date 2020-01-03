1b5ee82135653f5692a5edad40705d1ddb5b5ea9
wkdehdlr <wkdehdlr@gmail.com>
2020-01-03 14:43:50 +0900

    boot04-14-HttpMessageConverters * 스프링프레임워크에서 제공하는 인터페이스 * HTTP 요청 본문을 객체로 변경하거나, 객체를 HTTP 응답 본문으로 변경할 때 사용 * @RequestBody     * 요청 본문에 데이터가 들어있는데 이걸 내가 객체로 받고싶을때 * @ResponseBody * 우리가 어떤 요청을 받았는지, 어떤 응답을 보낼지에 따라 사용하는 HttpMessageConverters가 다르다.     * 가령 요청이 json요청이고 json본문이 들어왔다 -> jsonMessageConverter 사용해서 json메세지를 user라는 객체로 converting함     * User를 리턴해야하는데 User는 객체고 HTTP는 다 문자이기 때문에 객체를 그대로 응답으로 보낼수가 없어서 변환을 해야한다. 기본적으로는 jsonMessageConverter가 사용됨 -> 컴포지션타입일 경우     * 그냥 String일 경우 StringMessageConverter가 사용     * 클래스에 @RestController가 붙어있으면 @ResponseBody를 생략해도 된다.     * 그냥 @Controller인 경우에는 @ResponseBody를 명시해야함         * 리턴을 BeanNameViewResolver를 사용해서 이 리턴에 해당하는 뷰를 찾으려고 시도한다         * @RestController인 경우에는 BeanNameViewResolver를 타는게 아니라 MessageConverter를 타서 응답본문으로 내용이 들어간다

--- src/test/java/com/example/demo/user/UserControllerTest.java
+++ src/test/java/com/example/demo/user/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.demo.user;
 
-import com.example.demo.UserController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
