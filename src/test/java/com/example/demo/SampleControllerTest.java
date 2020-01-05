58b4b09ea19eadc1f25c97eeacb5c73489c438b0
wkdehdlr <wkdehdlr@gmail.com>
2020-01-05 16:30:36 +0900

    ㅁHtmlUnit * 의존성 추가 <dependency>    <groupId>org.seleniumhq.selenium</groupId>    <artifactId>htmlunit-driver</artifactId>    <scope>test</scope> </dependency> <dependency>    <groupId>net.sourceforge.htmlunit</groupId>    <artifactId>htmlunit</artifactId>    <scope>test</scope> </dependency>
    * 테스트에서 Webclient를 사용

--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -1,33 +1,28 @@
 package com.example.demo;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    WebClient webClient;
 
     @Test
     public void hello() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("hello"))
-                .andExpect(model().attribute("name",is("doik")))
-                .andExpect(content().string(containsString("doik")));
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("doik");
     }
 
 }
