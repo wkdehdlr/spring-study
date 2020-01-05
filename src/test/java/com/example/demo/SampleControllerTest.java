bcc56684cff55f823f1885ea294e79274e8eb292
wkdehdlr <wkdehdlr@gmail.com>
2020-01-05 16:12:14 +0900

    ㅁ템플릿 엔진 * 동적 컨텐츠 생성 * view만 만드는데 쓰이는게 아니다.(주로 view에 쓰임, 코드 제너레이션, 이메일 템플릿 등등) * 스프링 부트가 자동 설정을 지원하는 템플릿 엔진     * FreeMarker     * Groovy     * Thymeleaf     * Mustache * JSP는 권장하지 않음     * 스프링부트가 지향하는 방향과 거리가 있다     * 스프링부트는 독립적으로 실행가능한 임베디드 톰캣으로 어플리케이션을 빠르고 쉽게 배포     * JSP는 JAR로 패키징한 파일을 사용하지 못한다(?)     * WAR 패키징을 해야함 * Controller는 더이상 @RestController가 아니기 때문에 메소드 리턴값(String)이 메세지 본문이 아니다

---
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name",is("doik")))
+                .andExpect(content().string(containsString("doik")));
+    }
+
+}
