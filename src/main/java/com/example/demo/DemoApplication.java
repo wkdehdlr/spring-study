a97ad163fc042c347a5a8bafbdf5bf57b15b5248
wkdehdlr <wkdehdlr@gmail.com>
2020-02-02 19:33:06 +0900

    * 관계형 데이터베이스와 자바     * try with resources     * connection         * 만드는 비용이 비쌈         * DBCP로 관리(마음대로 생성하지 못함)     * sql이 DB마다 다름         * DB를 바꿨을때 sql이 다 바뀜     * Lazy loading 불가     * 따라서 ORM을 사용함

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,28 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(DemoApplication.class, args);
+
+		String url = "jdbc:postgresql://localhost:5432/springdata";
+		String userName = "doik";
+		String password = "pass";
+
+		try(Connection connection = DriverManager.getConnection(url, userName, password)){
+			System.out.println("Connection created ::" + connection);
+			String sql = "INSERT INTO VALUES(1, 'doik', 'pass')";
+			try(PreparedStatement psmt = connection.prepareStatement(sql)){
+				psmt.execute();
+			}
+		}
 	}
 
 }
