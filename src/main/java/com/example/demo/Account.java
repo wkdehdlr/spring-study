d0d23556eadc25a772060fa2ae30a3c473d0874c
wkdehdlr <wkdehdlr@gmail.com>
2020-02-02 21:53:58 +0900

    * JPA 프로그래밍 프로젝트 세팅     * spring.jpa.hibernate.ddl-auto         * create             * 보통 개발중일때 사용         * validate             * 이미 스키마는 만들어져있다는 가정하에 우리가 맵핑해야할 객체의 정보가 데이터베이스에 잘 매핑이 되는지 검증(새로만듬X)             * 매핑이 안되면 구동시에 에러남         * update             * 컬럼 추가는 되는데 삭제했을때 남아있음             * 기존 컬럼 이름을 변경하면 기존것도 남아있는상태로 새롭게 추가됨             * 스키마가 더러워질 수 있음     * @Entity         * 클래스가 데이터베이스에 있는 테이블과 매핑됨을 알려줌         * 멤버변수들은 테이블 컬럼에 매핑됨             * @Colunm 어노테이션이 생략됨     * EntityManager         * Jpa 핵심 클래스         * Entity들을 영속화 할 수 있음         * EntitiyManager와 관련된 모든 오퍼레이션들은 한 트랙잭션안에서 처리되어야 함(@Transactional)             * 메소드에만 붙일수도 있음

---
+++ src/main/java/com/example/demo/Account.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/demo/JpaRunner.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account account = new Account();
+        account.setName("doik");
+        account.setPassword("0725");
+        entityManager.persist(account);
+
+        Session session = entityManager.unwrap(Session.class);
+        session.save(account);
+    }
+}
