df49d5a7db10d6d01a2442e6c83f640ac6b8fbfd
wkdehdlr <wkdehdlr@gmail.com>
2020-02-05 09:04:03 +0900

    * JPA 소개 및 원리     * 원래는 @Configuration 클래스에 @EnableJpaRepositories 붙여야하지만 스프링부트가 해 줌     * JpaRepository<Entity, id> 인터페이스         * 인터페이스에 @Repository가 없어도 빈으로 등록해줌     * @EnableJpaRepositories         * @Import(JpaRepositoriesRegistrar.class)         * 핵심은 ImportBeanDefinitionRegistrar

---
+++ src/main/java/com/example/demo/DoinRegistrar.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class DoinRegistrar implements ImportBeanDefinitionRegistrar {
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry, BeanNameGenerator importBeanNameGenerator) {
+        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+        beanDefinition.setBeanClass(Post.class);
+
+        registry.registerBeanDefinition("postt",beanDefinition);
+
+    }
+}
--- src/main/java/com/example/demo/JpaRunner.java
+++ src/main/java/com/example/demo/JpaRunner.java
@@ -1,36 +1,19 @@
 package com.example.demo;
 
-import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 @Component
 @Transactional
 public class JpaRunner implements ApplicationRunner {
-    @PersistenceContext
-    EntityManager entityManager;
+    @Autowired
+    PostRepository postRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        Post post = new Post();
-        post.setTitle("Spring Data JPA 언제 보나.....");
-
-        Comment comment = new Comment();
-        comment.setComment("빨리봐야지");
-        post.addComment(comment);
-
-        Comment comment1 = new Comment();
-        comment1.setComment("곧 보여드릴게요");
-        post.addComment(comment1);
-
-        Session session = entityManager.unwrap(Session.class);
-        session.save(post);
-
+        postRepository.findAll().forEach(System.out::println);
     }
 }
