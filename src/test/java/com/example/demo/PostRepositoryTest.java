dccfb15b717005829459c58bdcf56896b0c95331
wkdehdlr <wkdehdlr@gmail.com>
2020-02-19 08:20:59 +0900

    * 스프링데이터 Common     * @NoRespositoryBean         * 중간단계 repository는 bean으로 등록하지 않는다     * @DataTest들은 기본적으로 @Transactional 붙어있기 때문에 롤백을 함

---
+++ src/test/java/com/example/demo/PostRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @Rollback(false)
+    public void crudRepository(){
+        Post post = new Post();
+        post.setTitle("hello spring boot common");
+        assertThat(post.getId()).isNull();
+
+        // When
+        Post newPost = postRepository.save(post);
+
+        // Then
+        assertThat(newPost.getId()).isNotNull();
+
+        // When
+        List<Post> postList = postRepository.findAll();
+
+        // Then
+        assertThat(postList.size()).isEqualTo(1);
+        assertThat(postList).contains(newPost);
+
+        // When
+        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+
+        // When
+        page = postRepository.findByTitleContains("spring", PageRequest.of(0, 10));
+
+        // Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+    }
+}
