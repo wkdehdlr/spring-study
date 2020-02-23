6cc7391e05f51cd287acc9e7b8a0ba798e8d8861
wkdehdlr <wkdehdlr@gmail.com>
2020-02-24 08:07:13 +0900

    * JPA 엔티티 저장하기         * Transient             * 하이버네이트, 데이터베이스 둘다 모름             * 새로 만들어진 객체         * Persistent             * PersistContext가 상태관리를 하는 상태             * Persist() 메소드에 넘긴 그 엔티티 객체를 Persistent상태로 변경         * Detached             * 한번이라도 Persistent가 됬던 객체             * Merge() 메소드에 넘긴 그 엔티티의 복사본을 만들고, 그 복사본을 다시 Persistent 상태로 변경하고 그 복사본을 반환

--- src/test/java/com/example/demo/AccountRepositoryTest.java
+++ src/test/java/com/example/demo/AccountRepositoryTest.java
@@ -1,16 +1,14 @@
 package com.example.demo;
 
 
-import com.querydsl.core.types.Predicate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Optional;
+import javax.persistence.EntityManager;
 
-import static com.example.demo.QAccount.account;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -20,14 +18,26 @@ public class AccountRepositoryTest {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    EntityManager entityManager;
+
     @Test
     public void crud(){
-        Predicate predicate = account
-                .username.containsIgnoreCase("doik")
-                .and(account.lastName.startsWith("jang"));
+        Account account = new Account();
+        account.setLastName("doik");
+        Account saveAccount = accountRepository.save(account);//persist
 
-        Optional<Account> one = accountRepository.findOne(predicate);
-        assertThat(one).isEmpty();
+        assertThat(entityManager.contains(account)).isTrue();
+        assertThat(entityManager.contains(saveAccount)).isTrue();
+        assertThat(saveAccount == account);
 
+        Account account2 = new Account();
+        account2.setId(account.getId());
+        account2.setLastName("doik");
+        Account updateAccount = accountRepository.save(account2);//merge
+
+        assertThat(entityManager.contains(account2)).isTrue();
+        assertThat(entityManager.contains(updateAccount)).isTrue();
+        assertThat(updateAccount == account2);
     }
 }
