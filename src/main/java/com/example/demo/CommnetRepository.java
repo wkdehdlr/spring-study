dd497314036fc821f5c4f6bd4a9d2e3c53cc6af5
wkdehdlr <wkdehdlr@gmail.com>
2020-02-23 14:16:28 +0900

    * 쿼리만들기실습

--- src/main/java/com/example/demo/CommnetRepository.java
+++ src/main/java/com/example/demo/CommnetRepository.java
@@ -2,11 +2,20 @@ package com.example.demo;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
+import java.util.concurrent.Future;
 
 public interface CommnetRepository extends MyRepository<Comment, Long>{
     List<Comment> findByCommentContains(String keyword);
+
+    Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
+
+    @Async
+    Future<List<Comment>> findByCommentContains(String keyword, Pageable pageable);
+
+    List<Comment> findByCommentOrderByLikeCountDesc();
 
     Page<Comment> findByLikeCountGreaterThanAndPost(int likeCount, Post post, Pageable pageable);
 }
--- src/test/java/com/example/demo/CommnetRepositoryTest.java
+++ src/test/java/com/example/demo/CommnetRepositoryTest.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.concurrent.Future;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,10 +24,24 @@ public class CommnetRepositoryTest {
 
     @Test
     public void crud(){
-        Optional<Comment> byId = commnetRepository.findById(100L);
-        assertThat(byId).isEmpty();
-        byId.orElseThrow(IllegalAccessError::new);
-        Comment comment = byId.orElse(new Comment());
-    }
+        Comment comment = new Comment();
+        comment.setComment("spring data jpa");
+        comment.setLikeCount(55);
+        commnetRepository.save(comment);
 
+        Comment comment2 = new Comment();
+        comment2.setComment("hibernate");
+        commnetRepository.save(comment2);
+
+        List<Comment> commentList = commnetRepository.findByCommentContains("Spring");
+        assertThat(commentList.size()).isEqualTo(1);
+
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "LikeCount"));
+        Page<Comment> comments = commnetRepository.findByCommentContainsIgnoreCase("Spring", pageRequest);
+        assertThat(comments.getNumberOfElements()).isEqualTo(2);
+        assertThat(comments).first().hasFieldOrPropertyWithValue("likeCount",100);
+
+        Future<List<Comment>> future =
+                commnetRepository.findByCommentContains("Spring",pageRequest);
+    }
 }
