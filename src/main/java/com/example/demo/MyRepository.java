5dcb6e5b9992be7009d10e630cce30213b9f013a
wkdehdlr <wkdehdlr@gmail.com>
2020-02-23 13:17:42 +0900

    * Null 처리     * 단일값은 Optional 쓰는걸 추천     * 콜렉션은 Null이 아니라 비어있는 콜렉션이 반환     * 스프링 프레임워크 5.0부터 지원하는 Null 어노테이션 지원         * @NonNullApi, @NonNull, @Nullable         * 런타임 체크 지원 함

--- src/main/java/com/example/demo/MyRepository.java
+++ src/main/java/com/example/demo/MyRepository.java
@@ -2,15 +2,21 @@ package com.example.demo;
 
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
-public interface MyRepository <T, id extends Serializable> extends Repository<T, id> {
-    <E extends T> E save(E entity);
+public interface MyRepository <T, Id extends Serializable> extends Repository<T, Id> {
+    <E extends T> E save(@NonNull  E entity);
 
     List<T> findAll();
+
+    @Nullable
+    <E extends T>Optional<E> findById(Id id);
 
     long count();
 }
--- src/test/java/com/example/demo/CommnetRepositoryTest.java
+++ src/test/java/com/example/demo/CommnetRepositoryTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,15 +20,10 @@ public class CommnetRepositoryTest {
 
     @Test
     public void crud(){
-        Comment comment = new Comment();
-        comment.setComment("hello Comment");
-        commnetRepository.save(comment);
-
-        List<Comment> all = commnetRepository.findAll();
-        assertThat(all.size()).isEqualTo(1);
-
-        long count = commnetRepository.count();
-        assertThat(count).isEqualTo(1);
+        Optional<Comment> byId = commnetRepository.findById(100L);
+        assertThat(byId).isEmpty();
+        byId.orElseThrow(IllegalAccessError::new);
+        Comment comment = byId.orElse(new Comment());
     }
 
 }
