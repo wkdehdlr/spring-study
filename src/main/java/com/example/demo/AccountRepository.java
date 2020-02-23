d6a53e92ffdbd979ad4cdf9edd634bf677c27609
wkdehdlr <wkdehdlr@gmail.com>
2020-02-24 08:23:27 +0900

    * Named Parameter와 SpEL

--- src/main/java/com/example/demo/AccountRepository.java
+++ src/main/java/com/example/demo/AccountRepository.java
@@ -4,12 +4,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long>, QuerydslPredicateExecutor<Account> {
     List<Account> findByUsernameStartsWith(String name);
 
-    @Query("SELECT a FROM Account AS a WHERE a.username = ?1")
-    List<Account> findByUsername(String name, Sort sort);
+    @Query("SELECT a FROM #{#entityName} AS a WHERE a.username = :title")
+    List<Account> findByUsername(@Param("title")String name, Sort sort);
 }
