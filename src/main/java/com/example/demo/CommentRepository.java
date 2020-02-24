e8709e910b5c2452825f5547a3a8b89e9c251887
wkdehdlr <wkdehdlr@gmail.com>
2020-02-24 09:03:13 +0900

    * Projection     * 엔티티의 일부 데이터만 가져오기

--- src/main/java/com/example/demo/CommentRepository.java
+++ src/main/java/com/example/demo/CommentRepository.java
@@ -11,5 +11,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @EntityGraph(value = "Comment.account")
     Optional<Comment> getById(Long id);
 
-    List<Comment> findByAccount_Id(Long id);
+    List<CommentSummary> findByAccount_Id(Long id);
 }
